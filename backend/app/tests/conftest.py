6b5a8995828d6329319a52ab070c5dadc04e1cc2
Bruno Hrgovic <bruno.hrgovic@gmail.com>
2026-02-19 12:11:32 +0900

    database creation in conftest

--- backend/app/tests/conftest.py
+++ backend/app/tests/conftest.py
@@ -1,6 +1,9 @@
 import pytest
 from fastapi.testclient import TestClient
 from app.main import app
+from app.database import Base, engine
+
+Base.metadata.create_all(bind=engine)
 
 client = TestClient(app)
 
